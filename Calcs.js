ed4c37939bb3d504fb71181f74795fce1f4c9e10
Jerrified <68245366+Jerrified@users.noreply.github.com>
2020-07-29 16:32:50 -0400

    Java 1st time

---
+++ Calcs.js
@@ -0,0 +1,21 @@
+// var x = 10;
+// var y = 2;
+// console.log("Code Differently");
+// console.log(3 + 4);
+// console.log(5 - 3);
+// console.log(3 * 3);
+// console.log(12 / 2);
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(y+x);
+// console.log(y-x);
+// console.log(y*x);
+// console.log(y/x);
+
+for(i=5; i <= 15; i+=2){
+    console.log(i);
+}
+
