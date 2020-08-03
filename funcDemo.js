d346dcdf807877756433bb07e79f6eddee88a318
Jerrified <68245366+Jerrified@users.noreply.github.com>
2020-08-03 15:31:47 -0400

    Added Javascript

---
+++ funcDemo.js
@@ -0,0 +1,33 @@
+
+function sum(x , y) {
+    return x + y;
+}
+
+console.log(sum(7 , 13));
+
+
+function subtract(a , b) {
+    return a - b;
+}
+
+console.log(subtract(15 , 78));
+
+
+function multiply(c , d) {
+    return c * d;
+}
+
+console.log(multiply(19 , 1972));
+
+
+function divide(e , t) {
+    return e / t;
+}
+
+console.log(divide(42 , 43));
+
+function power(s , m) {
+    return s ** m;
+}
+
+console.log(power(7 , 72))
---
+++ trafficLight.js
@@ -0,0 +1,23 @@
+var yllw = "Whoa, slow down.";
+var grn = "Go, go, go!";
+var rd = "STOP!";
+
+for (let i = 1; i <= 2; i++) {
+    
+    if (i == 0) {
+        console.log(grn);
+    }
+
+    else if (i == 1) {
+        console.log(yllw);
+    }
+
+    else if (i == 2) {
+        console.log(rd);
+    }
+
+    else {
+        consol.log("Drive with caution!")
+    }
+    
+}
