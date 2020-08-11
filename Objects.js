f05f85f0ec15c181fb00d2434b9c9af61a62c077
Jerrified <68245366+Jerrified@users.noreply.github.com>
2020-08-11 14:15:47 -0400

    Finished Penguin

--- Objects.js
+++ Objects.js
@@ -19,24 +19,37 @@ var myPenguin = {
     name: "Gunter",
     creator: "Pendolton Ward",
     origin: "Adventure Time",
-    habit: "Stealing",
-    greeting: function(){
-        return "Hello, I'm a penguin and my name is [NAME HERE]!";
-    },
-    canFly: false,
-    chirp: function(){
-        return "CHIRP CHIRP! Is this what penguins sound like?";
-    },
-    sayHello: function(){
-        return "Hello, I'm a penguin and my name is " + this.name + ".";
-    },
-    fly: function(){
-        if (canFly == true) {
-            "I can fly!";
-        } else {
-            "No flying for me!";
-        }
+}
+
+// console.log("Hello, I'm a penguin and my name is" +  " " + myPenguin.name + ".");
+
+myPenguin.canFly = false;
+
+// console.log(myPenguin.canFly);
+
+myPenguin.chirp = function(){
+    console.log("CHIRP CHIRP! Is this what penguins sound like?");
+}
+
+// console.log(myPenguin.chirp());
+
+myPenguin.sayHello = function(){
+    console.log("Hello, I'm a penguin and my name is" +  " " + this.name + ".");
+}
+
+myPenguin.name = "Penguin McPenguinFace";
+
+// console.log(myPenguin.sayHello());
+
+myPenguin.fly = function(){
+    if (this.canFly == true) {
+        console.log("I can fly!");
+    } else {
+        console.log("No flying for me!");
     }
 }
 
-console.log(myPenguin.sayHello());
+myPenguin.canFly = true;
+
+// console.log(myPenguin.fly());
+
